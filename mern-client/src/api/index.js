d1c2847b7726958c9d050fa42430bdd0e5b7b152
2003aia <62010027+2003aia@users.noreply.github.com>
2021-09-21 17:48:37 +0900

    api changed

--- mern-client/src/api/index.js
+++ mern-client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/posts';
+const url = 'https://warm-reef-74008.herokuapp.com/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
